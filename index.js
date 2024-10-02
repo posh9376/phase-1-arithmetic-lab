598849f5e0bf317ca1f3c9a5ff54d4cfe45ede73
Eric Richard <reric0802@gmail.com>
2024-10-02 12:05:04 +0300

    adds code to ensure passing

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 37
+let num4 = 11
+
+let mod = num3 % num4
+
+let max = Math.max(15, 16, 17, 18, 19, 20)
